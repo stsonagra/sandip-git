05a9b8a210d6b4c283d3dd4d567486b6c70fa8c3
stsonagra <153346519+stsonagra@users.noreply.github.com>
2024-11-10 21:00:19 +0530

    add customer controller
    for #4

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +20,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::resource('customer' , CustomerController::class);
